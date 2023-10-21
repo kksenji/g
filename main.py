a0c22ecfd56b96b5bae047963647728d2ac73708
Lenovo <avg090909@gmail.com>
2023-10-21 08:23:26 +0300

    V

---
+++ main.py
@@ -0,0 +1,19 @@
+
+#a = int(input())
+
+
+
+#if a % 2 <= 0:
+    #print("True")
+#else:
+    #print("False")
+
+
+#x = int(input())
+
+#f: int = 1
+
+#for i in range(2, x + 1):
+    #f *= i
+
+#print(f)
